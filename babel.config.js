53a9af31c2a71555c90d97d7ce9d9ae988e342d5
liujunjie <liujunjie@bbdservice.com>
2019-01-23 16:30:47 +0800

    update version

--- babel.config.js
+++ babel.config.js
@@ -1,23 +1,33 @@
-module.exports = function(api) {
-    api.cache(true);
+const ENV = {
+    DEVELOPMENT: 'development',
+    PRODUCTION: 'production',
+    TEST: 'test'
+};
+const MODE = {
+    ESM: 'esm',
+    COMMONJS: 'commonjs'
+};
 
+module.exports = function (api) {
+    api.cache(true);
+    
+    var env = process.env.NODE_ENV;
+    var mode = process.env.BABEL_ENV;
     var presets = [
         ['@babel/preset-env', {
             targets: [
                 'last 2 version',
                 'ie >= 9'
             ],
-            // useBuiltIns: 'entry',
-            modules: process.env.BABEL_ENV === 'esm' ? false : 'commonjs'     // transform esm to cjs, false to keep esm.
+            modules: mode === MODE.ESM ? false : MODE.COMMONJS     // transform esm to cjs, false to keep esm.
         }]
-        // '@babel/preset-react'
     ];
-
     var plugins = [
         '@babel/plugin-transform-runtime',
         '@babel/plugin-proposal-class-properties',
         '@babel/plugin-proposal-optional-chaining',
         '@babel/plugin-proposal-export-default-from',
+        '@babel/plugin-proposal-export-namespace-from',
         ['babel-plugin-module-resolver', {
             alias: {
                 '^config/(.+)': './src/_config/\\1',
@@ -31,19 +41,20 @@ module.exports = function(api) {
         // JS 引入的图片全部转成 base64格式, 避免打包后路径错误的问题.
         ['babel-plugin-inline-import-data-uri', {
             extensions: ['png', 'jpg', 'jpeg', 'gif', 'svg']
-        }],
+        }]
     ];
+
+    switch(env) {
+        case ENV.DEVELOPMENT:
+            break;
+        case ENV.PRODUCTION:        
+            break;
+        case ENV.TEST:
+            break;
+    }
 
     return {
         presets,
-        plugins,
-        env: {
-            test: {
-                presets: [
-                    '@babel/preset-env'
-                    // '@babel/preset-react'
-                ]
-            }
-        }
+        plugins
     };
 };
