5f6b68ce376179f3b51a3555398b369b0ebf4e00
liujunjie <liujunjie@bbdservice.com>
2019-09-25 19:09:42 +0800

    更新中

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,11 @@
 var autoprefixer = require('autoprefixer');
+var vars = require('postcss-simple-vars');
 var nested = require('postcss-nested');
 
 module.exports = {
     plugins: [
         autoprefixer,
+        vars,
         nested()
     ]
 };
