1a5fa00eca8aba0fb3f785e3a467070968d42424
Stephen <stephenliu1944@outlook.com>
2020-01-28 13:43:18 +0800

    更新测试代码

--- test/app.js
+++ test/app.js
@@ -4,4 +4,4 @@ import { Component1, Component2 } from '../src/index';
 var m1 = new Component1();
 var m2 = new Component2();
 
-document.body.innerHTML = `<div>${ m1.render() + m2.render() }</div>`;
+document.querySelector('#app').innerHTML = `<div>${ m1.render() + m2.render() }</div>`;
