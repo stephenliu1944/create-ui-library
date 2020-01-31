6e7c75d4e5f13bd8a13373123de52e8b4bcf233c
Stephen <stephenliu1944@outlook.com>
2020-02-01 02:01:04 +0800

    test

--- src/component2/Component2.js
+++ src/component2/Component2.js
@@ -1,6 +1,9 @@
 import fatPNG from './images/fat.png';
 
 export default function() {
+    var a = 1;
+    var b = 2;
+    
     return `
         <div class="component2">
             <img src="${ fatPNG }" />
