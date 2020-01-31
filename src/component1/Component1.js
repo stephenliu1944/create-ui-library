e78bcd3d4e0fdaf0709978c7cffba89f6f5ae27b
Stephen <stephenliu1944@outlook.com>
2020-02-01 02:18:51 +0800

    test

--- src/component1/Component1.js
+++ src/component1/Component1.js
@@ -2,10 +2,10 @@ import alienPNG from './images/alien.png';
 
 export default function() {
     return `
-        <div class="component1">
+        <h1 class="component1">
             Component1
             <img src="${ alienPNG }" />
             <p>Hello World</p>
-        </div>
+        </h1>
     `;
 }
